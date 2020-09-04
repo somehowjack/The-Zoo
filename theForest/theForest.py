061dd88f8cf02a36065a6af6552645be6927bcf9
William <bulworm@gmail.com>
2020-09-04 21:52:13 +0000

    oaoaoaoaoaoaoao

--- theForest/theForest.py
+++ theForest/theForest.py
@@ -1,6 +1,6 @@
-path1 = {"index":1, "text":"you're in the same spot that you were in when you came here", "options": ["move forward"], "move forward": ["changeIndex", 2]}
+path1 = {"index":1, "new?":True, "text1":"you're on the end of a path, or maybe it's beggining. in the distance you can see small vortxes of leaves wander the forest before falling to the ground.", "text2":"you're at the begining of the path.", "options": ["move forward"], "move forward": ["changeIndex", 2]}
 
-path2 = {"index":2, "text":"sampletext", "options":["move forward", "move backward"], "move forward": [], "move backward": []}
+path2 = {"index":2, "new?":True, "text1":"sampletext", "text2":"sampletext", "options":["move forward", "move backward"], "move backward": ["changeIndex", 1], "move forward": ["changeIndex", 3]}
 
 pathDir = [path1, path2]
 
@@ -13,12 +13,17 @@ def mainPath():
     for dic in pathDir:
       if dic["index"] == indexCurr:
         indexCont = dic
-    print(indexCont["text"])
+    if indexCont["new?"]:
+      print(indexCont["text1"])
+      indexCont["new?"] = False
+    else:
+      print(indexCont["text2"])
+    
     for all in indexCont["options"]:
       print(all)
     option = input("\nwhat do you do? ")
     while option not in indexCont["options"]:
-      option = input("what do you do?")
+      option = input("what do you do? ")
 
     if indexCont[option][0] == "changeIndex":
       indexCurr = indexCont[option][1]
