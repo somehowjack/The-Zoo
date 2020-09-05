fab89b35fcbc9a1e0741add545fe59e21f02f3ac
William <bulworm@gmail.com>
2020-09-05 02:18:47 +0000

    vfkvbfkvhbdkvhbdfkhvb

--- theForest/theForest.py
+++ theForest/theForest.py
@@ -7,6 +7,12 @@ path1 = {"index":1, "new?":True, "text1":"\nyou wake up on the end of a path, or
 
 path2 = {"index":2, "new?":True, "text1":"\ncontinuing on the path you smell the air around you, it smells like ash but slowly your nose gets used to the smell. you stop in front of a sign with the words 'welcome to the forest' scawled onto it's face. on it is pinned a note.\n", "text2":"\nyou're at a sign that says 'welcome to the forest' with a note pinned to it.\n", "options":["keep moving forward", "go backward", "read note"], "go backward": ["changeIndex", 1], "keep moving forward": ["changeIndex", 3], "read note": ["printFile", "seen/trees/notes/path2.txt"]}
 
+path3 = {"index":3, "new?":True, "text1":"\nthe path turns where a tree stands. the tree is prefectly dead. every leaf is gone and the branches don't look too sturdy. looking up you can see something at the top and a note carved into the lowest branch.\n", "text2":"\nsampletext\n", "options":["keep moving forward", "go backward", "read note", "climb tree"], "keep moving forward":["changeIndex", 8], "go backward":["changeIndex", 2], "read note":["printFile", "seen/trees/notes/path3.txt"], "climb tree":["newIndex", 4]}
+
+tree1 = {"index":4, "new?":True, "text1":"\nsampletext\n", "text2":"\nsampletext\n", "options":[], "":[]}
+
+path4 = {"index":8, "new?":True, "text1":"\nsampletext\n", "text2":"\nsampletext\n", "options":[], "":[]}
+
 pathDir = [path1, path2]
 
 def mainPath():
